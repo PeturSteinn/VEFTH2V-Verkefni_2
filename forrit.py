d242e651d1c15c4927fdf5a0ff540b6272def187
PeturSteinn <peturgu220@nemi.tskoli.is>
2018-01-24 20:15:36 +0000

    Laga run()

--- forrit.py
+++ forrit.py
@@ -68,7 +68,8 @@ def error404(error):
            <h3><a href="/">Á heimasíðu</a></h3>
            '''
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=os.environ.get('PORT'))
+#if os.environ.get('APP_LOCATION') == 'heroku':
+#    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#else:
+#    run(host='localhost', port=8080, debug=True)
